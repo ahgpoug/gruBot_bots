581dbec707f756e3c67f3a31249a219e1aab99ad
ahgpoug <maxkost5@gmail.com>
2018-03-10 16:00:32 +0300

    Change announcements regexp

--- src/main/java/gruBot/telegram/bot/GruBotPatterns.java
+++ src/main/java/gruBot/telegram/bot/GruBotPatterns.java
@@ -1,7 +1,7 @@
 package gruBot.telegram.bot;
 
 public class GruBotPatterns {
-    public static final String announcement = "^\\![^!\\r\\n]+\\!\\n[^!\\r\\n]+$";
+    public static final String announcement = "^\\![^!\\r\\n]+\\!\\n[^.]+$";
     public static final String announcementTitle = "^\\![^!\\r\\n]+\\!\\n";
-    public static final String announcementText = "[^!\\r\\n]+$";
+    public static final String announcementText = "\\n[^.]+$";
 }
